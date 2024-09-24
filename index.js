06a58b17b6cd1e56e0515c7cbb3e0c2be1261c5c
John Ferdie Abueg <john.abueg@gmail.com>
2024-09-24 21:53:20 +0200

    core

--- index.js
+++ index.js
@@ -53,3 +53,161 @@ const state = {
   ],
   cart: []
 };
+
+const storeItemList = document.querySelector('.store--item-list');
+const cartItemList = document.querySelector('.cart--item-list')
+const total = document.querySelector('.total-number')
+
+function renderStoreItems() {
+  storeItemList.innerHTML = '';
+
+   state.items.forEach((item) => {
+    /*
+      <li>
+        <div class="store--item-icon">
+          <img src="assets/icons/001-beetroot.svg" alt="beetroot" />
+        </div>
+
+        <button>Add to cart</button>
+      </li>
+    */
+
+    const li = document.createElement('li');
+
+    // div
+    const divIcon = document.createElement('div');
+    divIcon.className = 'store--item-icon';
+    li.appendChild(divIcon);
+
+    // img
+    const img = document.createElement('img');
+    img.src = `assets/icons/${item.id}.svg`;
+    img.alt = item.name;
+    divIcon.appendChild(img);
+    
+
+    // button
+    const button = document.createElement('button');
+    button.textContent = 'Add to cart';
+    li.appendChild(button);
+
+    button.addEventListener('click', () => {
+      addItemToCart(item);
+    });
+
+    storeItemList.appendChild(li);
+  });
+}
+
+function renderCartItems() {
+  cartItemList.innerHTML = '';
+
+  state.cart.forEach((item) => {
+    /*
+      <li>
+        <img
+          class="cart--item-icon"
+          src="assets/icons/001-beetroot.svg"
+          alt="beetroot"
+        />
+        
+        <p>beetroot</p>
+        <button class="quantity-btn remove-btn center">-</button>
+        <span class="quantity-text center">1</span>
+        <button class="quantity-btn add-btn center">+</button>
+      </li>
+    */
+
+    const li = document.createElement('li');
+
+    // img
+    const img = document.createElement('img');
+    img.className = 'cart--item-icon'
+    img.src = `assets/icons/${item.id}.svg`
+    img.alt = `${item.name}`
+    li.appendChild(img)
+
+    // p
+    const p = document.createElement('p');
+    p.textContent = item.name
+    li.appendChild(p)
+
+    // decrement button
+    const decrementButton = document.createElement('button');
+    decrementButton.className = "quantity-btn remove-btn center"
+    decrementButton.textContent = '-'
+    decrementButton.addEventListener('click', () => {
+      decrementItem(item);
+    });
+    li.appendChild(decrementButton)
+
+    // span
+    const span = document.createElement('span');
+    span.className = "quantity-text center"
+    span.textContent = item.quantity
+    li.appendChild(span)
+
+    // increment button
+    const incrementButton = document.createElement('button');
+    incrementButton.className = "quantity-btn add-btn center"
+    incrementButton.textContent = '+'
+    incrementButton.addEventListener('click', () => {
+      incrementItem(item);
+    });
+    li.appendChild(incrementButton)
+
+    cartItemList.appendChild(li)
+  });
+}
+
+function addItemToCart(item) {
+  const cartItem = state.cart.find((cartItem) => cartItem.id === item.id);
+
+  if (cartItem) {
+      cartItem.quantity++;
+  } else {
+      state.cart.push({
+        ...item,
+        quantity: 1
+      });
+  }
+
+  renderCartItems();
+  calculateTotal();
+}
+
+function decrementItem(itemInCart) {
+  const targetItem = state.cart.find((item) => item.id === itemInCart.id);
+  targetItem.quantity--;
+
+  if(targetItem.quantity === 0){
+    const index = state.cart.findIndex((item) => item.id === itemInCart.id);
+    state.cart.splice(index, 1);
+    delete targetItem.quantity;
+  }
+
+  renderCartItems();
+  calculateTotal();
+}
+
+function incrementItem(itemInCart) {
+  const targetItem = state.cart.find((item) => item.id === itemInCart.id);
+  targetItem.quantity++;
+
+  renderCartItems();
+  calculateTotal();
+}
+
+function calculateTotal() {
+  let totalAmount = 0; 
+
+  state.cart.forEach((item) => {
+      totalAmount += item.price * item.quantity;
+  })
+
+  total.textContent = `£${totalAmount.toFixed(2)}`;
+}
+
+renderStoreItems();
+renderCartItems();
+calculateTotal();
